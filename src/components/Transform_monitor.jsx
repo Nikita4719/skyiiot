87e03ad829450ec07b2ce96591c91758aa924029
Nikita4719 <nikita@skylabstech.com>
2026-03-25 16:12:01 +0530

    transfer monitor

--- src/components/Transform_monitor.jsx
+++ src/components/Transform_monitor.jsx
@@ -62,7 +62,7 @@ export default function TransformMonitor() {
       )}
 
       {/* Main Section */}
-      <section className="py-5 bg-light">
+      <section className="py-5 bg-white">
         <div className="container">
           <div className="row g-4 align-items-start">
 
