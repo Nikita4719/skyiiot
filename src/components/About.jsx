f5a4db0ce440d2d9a8d077765cd8f2d363284e94
Nikita4719 <nikita@skylabstech.com>
2026-04-17 12:41:36 +0530

    abt img shifted to rightside

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -71,7 +71,7 @@ export default function About() {
                 <div className="col-md-6 text-center text-md-end">
                   <img
                     src={aboutusData?.image2 ? `${ROOT_URL}/${aboutusData.image2}` : "null"}
-                     className="img-fluid rounded ms-md-5 ms-2"
+                     className="img-fluid rounded ms-md-3 ms-3 about-img"
                     alt="About SkyIIOT"
                     style={{ maxWidth: "90%" }}
                   />
